7a2a736d17de0612a0a185ee8f03a6010fce182a
CoraHyunJung <cora.hyun.jung@gmail.com>
2019-06-05 16:33:56 +0900

    make fizzbuzz algorithm

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,13 @@
-for i in range(5):
-    print("Python works!")
+n = int(input(" Insert the number: "))
+for i in range(n):
+    if n%3 ==0:
+        print("fizz")
+    elif n%5==0:
+        print("buzz")
+    elif n%15==0:
+        print("fizzbuzz")
+    else:
+        print(i)
+
 
 
