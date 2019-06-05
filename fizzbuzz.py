4f8525443cef0ed1bfd112b58021d1c5b4dd8392
CoraHyunJung <cora.hyun.jung@gmail.com>
2019-06-05 16:37:30 +0900

    feat: add algorithm for fizzbuzz
    This commit is real fizzbuzz!
    I will remake this fizzbuzz with list comprehension.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,10 @@
 n = int(input(" Insert the number: "))
 for i in range(n):
-    if n%3 ==0:
+    if i%3 ==0:
         print("fizz")
-    elif n%5==0:
+    elif i%5==0:
         print("buzz")
-    elif n%15==0:
+    elif i%15==0:
         print("fizzbuzz")
     else:
         print(i)
