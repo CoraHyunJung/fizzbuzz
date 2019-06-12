9843ce80b82bde524022a02af7ec03a4f74448b2
CoraHyunJung <cora.hyun.jung@gmail.com>
2019-06-12 11:32:59 +0900

    fix: remake fizzbuzz function
    adopt ternary operator and list comprehension

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,3 @@
-n = int(input(" Insert the number: "))
-for i in range(n):
-    if i%3 ==0:
-        print("fizz")
-    elif i%5==0:
-        print("buzz")
-    elif i%15==0:
-        print("fizzbuzz")
-    else:
-        print(i)
+n = int(input(' Insert the number: '))
 
-
-
+print(['fizz' if i%3 == 0 else 'buzz' if i%5 == 0 else for i in range(1, n+1)])
