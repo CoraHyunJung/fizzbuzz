5148905c55d8aaf1b4ad0f104c6b8445e8c41438
CoraHyunJung <cora.hyun.jung@gmail.com>
2019-06-12 11:40:28 +0900

    fix: complete fizzbuzz function
    fiz and complete codes using ternary operator and list comprehension

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,3 @@
 n = int(input(' Insert the number: '))
 
-print(['fizz' if i%3 == 0 else 'buzz' if i%5 == 0 else for i in range(1, n+1)])
+print(['fizz' if i%3 == 0 else 'buzz' if i%5 == 0 else 'fizzbuzz' if i%15 == 0 else i for i in range(1, n+1)])
